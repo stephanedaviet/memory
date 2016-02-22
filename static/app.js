b5d686461364364b5bce4b42c940e035d3b8af5e
Stéphane Daviet <stephane.daviet@serli.com>
2016-02-22 14:42:15 +0000

    Deploying latest built version to gh-pages

---
+++ static/app.js
@@ -0,0 +1,2 @@
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?exports.App=t():e.App=t()}(this,function(){return function(e){function t(n){if(o[n])return o[n].exports;var r=o[n]={exports:{},id:n,loaded:!1};return e[n].call(r.exports,r,r.exports,t),r.loaded=!0,r.exports}var o={};return t.m=e,t.c=o,t.p="/static/",t(0)}([function(e,t,o){!function(){throw new Error('Cannot find module "./src/main.js"')}()}])});
+//# sourceMappingURL=app.js.map
